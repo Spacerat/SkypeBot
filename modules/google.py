b0bf9a85e0363b20b659c1ece2b2b87237959f9e
Spacerat <spacerat3004@gmail.com>
2010-04-08 00:24:33 +0100

    Made googlebot work with stringsafety

--- modules/google.py
+++ modules/google.py
@@ -2,21 +2,20 @@
 import robot
 import urllib2
 import json
-import codecs
+from stringsafety import *
 
 class GoogleRobot(robot.SkypeRobot):
     def OnInit(self):
         self.Name = "GoogleBot"
     def Handle(self,command,args):
         # @type args str
-        args = args.replace(" ","%20")
-        
-        url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="+args
+        url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="+URLSafe(args)
         request = urllib2.Request(url,None,{'Referer':'http://spacerat.meteornet.net'})
         response = urllib2.urlopen(request)
         results = json.load(response)
         self.Reply(results["responseData"]["results"][0]["url"])
-        self.Reply(results["responseData"]["results"][0]["content"])
+        content = results["responseData"]["results"][0]["content"]        
+        self.Reply(FormatHTML(content))
 
 robot.AddHook("google",GoogleRobot)
 
