41b3c09372f48d7ab9229abad5ff96661ed0804b
Spacerat <spacerat3004@gmail.com>
2010-04-07 18:10:34 +0100

    Made googlebot work

--- modules/google.py
+++ modules/google.py
@@ -2,19 +2,21 @@
 import robot
 import urllib2
 import json
+import codecs
 
 class GoogleRobot(robot.SkypeRobot):
     def OnInit(self):
         self.Name = "GoogleBot"
     def Handle(self,command,args):
         # @type args str
-        args.replace(" ","%20")
+        args = args.replace(" ","%20")
+        
         url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="+args
         request = urllib2.Request(url,None,{'Referer':'http://spacerat.meteornet.net'})
         response = urllib2.urlopen(request)
         results = json.load(response)
-        for x in results.responseData:
-            self.Reply(x)
+        self.Reply(results["responseData"]["results"][0]["url"])
+        self.Reply(results["responseData"]["results"][0]["content"])
 
 robot.AddHook("google",GoogleRobot)
 
