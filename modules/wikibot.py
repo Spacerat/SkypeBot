01ad5b9a9a6b85e6e1cdd5655177989b7048d934
Spacerat <spacerat3004@gmail.com>
2010-04-08 00:26:03 +0100

    Some work on wiki bot

--- modules/wikibot.py
+++ modules/wikibot.py
@@ -15,8 +15,10 @@ class WikiRobot(robot.SkypeRobot):
         request = urllib2.Request(url,None,{'Referer':'http://spacerat.meteornet.net'})
         response = urllib2.urlopen(request)
         results = json.load(response)
-        for page in results["query"]["pages"]:
-            self.Reply(results["query"]["pages"][page]["revisions"][0]['*'].encode('utf-8').partition(chr(10))[0])
-
+        if "pages" in results["query"]:
+            for page in results["query"]["pages"]:
+                self.Reply(results["query"]["pages"][page]["revisions"][0]['*'].encode('utf-8').partition(chr(10))[0])
+        else:
+            self.Reply("Wikipedia page for "+args+" does not exit.")
 robot.AddHook("wiki",WikiRobot)
 
