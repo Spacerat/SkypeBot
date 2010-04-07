e0c8d301cab4c53ff6fb9e168b0de554e02d3500
Spacerat <spacerat3004@gmail.com>
2010-04-07 13:09:12 +0100

    First skypebot commit

---
+++ skypebot.py
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------------------------------
+#  Python / Skype4Py example that prints out chat messages
+#
+#  Tested with  Skype4Py version 0.9.28.5 and Skype verson 3.8.0.96
+
+import Skype4Py
+import modules
+
+# ----------------------------------------------------------------------------------------------------
+# Fired on attachment status change. Here used to re-attach this script to Skype in case attachment is lost. Just in case.
+def OnAttach(status):
+    print 'API attachment status: ' + skype.Convert.AttachmentStatusToText(status)
+    if status == Skype4Py.apiAttachAvailable:
+        skype.Attach();
+
+    if status == Skype4Py.apiAttachSuccess:
+       print('******************************************************************************');
+
+# ----------------------------------------------------------------------------------------------------
+# Fired on chat message status change.
+# Statuses can be: 'UNKNOWN' 'SENDING' 'SENT' 'RECEIVED' 'READ'
+
+def OnMessageStatus(Message, Status):
+    modules.RecieveMessage(Message, Status)
+    
+# ----------------------------------------------------------------------------------------------------
+# Creating instance of Skype object, assigning handler functions and attaching to Skype.
+skype = Skype4Py.Skype()
+print "skype"
+skype.OnAttachmentStatus = OnAttach;
+skype.OnMessageStatus = OnMessageStatus;
+
+print('******************************************************************************');
+print 'Connecting to Skype..'
+skype.Attach()
+
+# ----------------------------------------------------------------------------------------------------
+# Looping until user types 'exit'
+Cmd = '';
+while not Cmd == 'exit':
+    Cmd = raw_input('');
