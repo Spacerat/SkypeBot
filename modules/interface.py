2349c8f84ad223989ea3e8a22bcee0452402d53c
Spacerat <spacerat3004@gmail.com>
2010-04-20 00:39:18 +0100

    Added a generic MessageHook, for functions which hook ALL messages.

--- modules/interface.py
+++ modules/interface.py
@@ -1,13 +1,23 @@
 
+class MessageHook:
+
+    Hooks = []
+
+    def __init__(self,hook):
+        self.Hook = hook
+        MessageHook.Hooks.append(self)
+
 class ComHook:
 
     Hooks = {}
 
-    def __init__(self,command,hook,name='',status='ANY'):
+    def __init__(self,command,hook,name='',status='ANY',prefix="!"):
         self.Name = name
         self.MStatus = status
         self.Hook = hook
+        self.prefix="!"
         ComHook.Hooks[command]=self
+
 
 #This function is now essentially redundant. Yay!
 def AddHook(command,hook,name='',status='ANY'):
@@ -15,12 +25,19 @@ def AddHook(command,hook,name='',status='ANY'):
 
 def RecieveMessage(Interface,text,MessageStatus):
 
-    if text!="" and (text[0]=="!"):
+    if text!="":
         command =  text.partition(" ")[0][1:len(text)]
         body = text.partition(" ")[2]
+        
+        #Super hooks
+        if (MessageStatus =='SENT' or MessageStatus == 'RECEIVED'):
+            for hook in MessageHook.Hooks:
+                hook.Hook(Interface,text)
+
+        #Command hooks
         if command in ComHook.Hooks:
             mtype = ComHook.Hooks[command].MStatus
-            if (mtype=='ANY' and (MessageStatus =='SENT' or MessageStatus == 'RECEIVED')) or MessageStatus == mtype:
+            if ((mtype=='ANY' and (MessageStatus =='SENT' or MessageStatus == 'RECEIVED')) or MessageStatus == mtype) and text.startswith(ComHook.Hooks[command].prefix):
                 Interface.Name = ComHook.Hooks[command].Name
                 hook = ComHook.Hooks[command].Hook
                 hook(Interface,command,body,MessageStatus)
