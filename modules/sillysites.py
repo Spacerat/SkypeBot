37b30ff71202a185a5c3b6a835b3fdc29b573051
Spacerat <spacerat3004@gmail.com>
2010-04-21 02:03:55 +0100

    Added !buuurn. Sillysites also now replaces the command where possible.

--- modules/sillysites.py
+++ modules/sillysites.py
@@ -2,11 +2,20 @@
 import interface
 
 def Handle(interface,command,args,messagetype):
+    rep=''
     if command=='rimshot':
-        interface.Reply(r'http://instantrimshot.com/')
-    elif command=='trombone':
-        interface.Reply(r'http://www.sadtrombone.com/')
 
+        rep=r'http://instantrimshot.com/'
+    elif command=='trombone':
+        rep=r'http://www.sadtrombone.com/'
+    elif command=='buuurn':
+        rep=r'http://buuurn.com/'
+
+    try:
+        interface.Message.Body = rep
+    except:
+        interface.Reply(rep)
 
 interface.AddHook("rimshot",Handle)
 interface.AddHook("trombone",Handle)
+interface.AddHook("buuurn",Handle)
