12df931fad3f2d18cc64dc10be4376d1e5b06b1a
Spacerat <spacerat3004@gmail.com>
2010-04-25 11:05:14 +0100

    Added !nooo

--- modules/sillysites.py
+++ modules/sillysites.py
@@ -8,7 +8,10 @@ def Handle(interface,command,args,messagetype):
         interface.Reply(r'http://www.sadtrombone.com/',edit=True)
     elif command=='buuurn':
         interface.Reply(r'http://buuurn.com/',edit=True)
+    elif command=='nooo':
+        interface.Reply(r'http://www.nooooooooooooooo.com/',edit=True)
 
 interface.AddHook("rimshot",Handle)
 interface.AddHook("trombone",Handle)
 interface.AddHook("buuurn",Handle)
+interface.AddHook("nooo",Handle)
