a7d37b227f5bd1c502e834f298b03c7eebc9d83c
Spacerat <spacerat3004@gmail.com>
2010-05-11 21:25:32 +0100

    !cls now uses status='SENT' for security.

--- modules/clearmessages.py
+++ modules/clearmessages.py
@@ -2,11 +2,10 @@
 import interface
 
 def Handle(interface,command,args,messagetype):
-    if messagetype=='SENT':
-        for x in interface.Message.Chat.Messages[0:int(args)]:
-            try:
-                x.Body=""
-            except:
-                pass
+    for x in interface.Message.Chat.Messages[0:int(args)]:
+        try:
+            x.Body=""
+        except:
+            pass
 
-interface.ComHook("cls",Handle,hidden=True)
+interface.ComHook("cls",Handle,hidden=True,status='SENT')
