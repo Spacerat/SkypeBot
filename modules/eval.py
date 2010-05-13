a684024b88fc06f78a90b3ffb1da0829557a3a54
Spacerat <spacerat3004@gmail.com>
2010-05-13 20:14:31 +0100

    Lots of changes:
    Reorganised the module. eval and run now have separate handlers, and separate thread objects.
    !eval renamed to !ev
    Eval now evaluates inline expressions in messages, designated by {"curley brackets"}.
    An example of this would be !ev There are {60*60*24} seconds in a day.
    Eval should resort to standard replies if it cannot edit a message. In this case, the function works as before.

--- modules/eval.py
+++ modules/eval.py
@@ -7,76 +7,114 @@ import skype
 import re
 import random
 
-def Handle(interface,command,args,messagetype):
-    if command == 'run':
-        if messagetype=='SENT':
-                ExecThread(args,interface,type='run').start()
-        else:
-            interface.Reply("Permission denied!")
+def RunHandle(interface,command,args,messagetype):
+    if messagetype=='SENT':
+            RunThread(interface,args).start()
+    else:
+        interface.Reply("Permission denied!")
 
-    if command=='eval':
-            ExecThread(args,interface,type='eval').start()
 
-class ExecThread(threading.Thread):
-    def __init__(self,code,interface,type='eval'):
+def EvalHandle(interface,command,args,messagetype):
+
+    lock = threading.Lock()
+    editing = True
+    try:
+        interface.Message.Body = args
+    except:
+        editing = False
+
+    if editing:
+        while True:
+            l = args.find("{")
+            if l>=0:
+                r = args.find("}")
+                sect=args[l:(r+1)]
+                code = args[(l+1):r]
+                ExecThread(interface,sect,code,mode='edit',lock=lock).start()
+                args = args[(r+1):len(args)]
+            else:
+                break
+    else:
+        ExecThread(interface,args,args,mode='reply')
+
+class RunThread(threading.Thread):
+    def __init__(self,interface,code):
         threading.Thread.__init__(self)
         self.i = interface
         self.code = code
-        self.type = type
+    def run(self):
+        i = self.i
+        r = self.r
+        exec self.code
+
+
+class ExecThread(threading.Thread):
+    def __init__(self,interface,replace,code,mode='reply',lock=None):
+        threading.Thread.__init__(self)
+        self.i = interface
+        self.code = code
+        self.replace = replace
+        self.lock = lock
+        self.mode = mode
 
     def run(self):
-        if self.type=='run':
-            i = self.i
-            r = self.r
-            exec self.code
-        elif self.type=='eval':
-            try:
-                locals = {
-                    #Math
-                    'pow': pow,
-                    'ceil': math.ceil,
-                    'floor': math.floor,
-                    'round': round,
-                    'log': math.log,
-                    'log10': math.log10,
-                    'e': math.e,
-                    'pi': math.pi,
-                    'sqrt': math.sqrt,
-                    'complex': complex,
-                    'abs': abs,
 
-                    #Lists
-                    'range': range,
-                    'map': map,
-                    'filter': filter,
-                    'reduce': reduce,
-                    'zip': zip,
-                    'enumerate': enumerate,
-                    'any': any,
-                    'all': all,
-                    
-                    #String
-                    'str': str,
-                    'chr': chr,
-                    'bin': bin,
+        try:
+            locals = {
+                #Math
+                'pow': pow,
+                'ceil': math.ceil,
+                'floor': math.floor,
+                'round': round,
+                'log': math.log,
+                'log10': math.log10,
+                'e': math.e,
+                'pi': math.pi,
+                'sqrt': math.sqrt,
+                'complex': complex,
+                'abs': abs,
 
-                    #Interface
-                    'i': self.i,
-                    
-                    '__builtins__': None,
-                }
-                locals['l']=locals
-                r=eval(self.code,{'__builtins__':None,},locals)
-                del locals['l']
-                s=str(r)
-                if len(s)>400: s = s[0:400]+" ..."
+                #Lists
+                'range': range,
+                'map': map,
+                'filter': filter,
+                'reduce': reduce,
+                'zip': zip,
+                'enumerate': enumerate,
+                'any': any,
+                'all': all,
+
+                #String
+                'str': str,
+                'chr': chr,
+                'bin': bin,
+
+                #Interface
+                'i': self.i,
+
+                '__builtins__': None,
+            }
+            locals['l']=locals
+            r=eval(self.code,{'__builtins__':None,},locals)
+            del locals['l']
+            s=str(r)
+            if len(s)>400: s = s[0:400]+" ..."
+            if mode=='edit':
+                if (self.lock):
+                    self.lock.acquire()
+                self.i.Message.Body = self.i.Message.Body.replace(self.replace,s)
+                if (self.lock):
+                    self.lock.release()
+            elif mode=='reply':
                 self.i.Reply(s)
-            except Exception as e:
-                self.i.Reply("Error: "+str(e))
+
+        except None as e:
+            #raise e
+            self.i.Reply("Error: "+str(e))
 
     def r(self,text):
         if text=='': return
         self.i.Reply(text)
 
-interface.ComHook('run',Handle,hidden=True)
-interface.ComHook('eval',Handle,name="EvalBot")
+interface.ComHook('run',RunHandle,hidden=True)
+interface.ComHook('ev',EvalHandle,name="EvalBot")
