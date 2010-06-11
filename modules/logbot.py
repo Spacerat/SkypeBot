6317ced38c178b72f2c3317285ac1fae3c3ddfbc
Spacerat <spacerat3004@gmail.com>
2010-06-11 13:32:35 +0100

    Added logbot for testing.

---
+++ modules/logbot.py
@@ -0,0 +1,13 @@
+
+import interface
+
+def Handle(interface,text):
+    f = open("log.txt","a+")
+    f.write(interface.UserName+"\n")
+    f.write(interface.UserAddress+"\n")
+    f.write(text+"\n")
+    f.close()
+
+    text = text.replace("\r\n\r\n<<< ","")
+    print text
+interface.MessageHook(Handle)
