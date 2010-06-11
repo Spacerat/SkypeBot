5352888ac641cdbb26ee400b8945147981aa8506
Spacerat <spacerat3004@gmail.com>
2010-06-11 13:32:10 +0100

    Re-imagined correctbot.
    Correct bot now uses a dictionary of words to replace with other words, which can be updated by chat users. It correctly replaces words in uppercase and titlecase as well as standard lowercase.

--- modules/correctbot.py
+++ modules/correctbot.py
@@ -4,13 +4,37 @@ from stringsafety import *
 import enchant
 from enchant.tokenize import get_tokenizer, EmailFilter, URLFilter
 from random import randint
+import json
 
 d = enchant.Dict("en_UK")
 tkn = get_tokenizer("en_UK",filters=[EmailFilter, URLFilter])
-enabled=False
+enabled=True
+
+def LoadReplaceDict(url=""):
+    global replaceurl
+    global repdict
+
+    if url!="": replaceurl = url
+    f = open(replaceurl)
+    if f:
+        repdict = json.load(f)
+        f.close()
+
+def SaveReplaceDict(url=""):
+    global replaceurl
+
+    if url!="": replaceurl = url
+    f = open(replaceurl,"w")
+    json.dump(repdict,f)
+    f.close()
+
+
+
 
 def Handle(interface,text):
 
+    if "ReplaceBot:" in text: return
+    '''
     if enabled:
         if not interface.Message.IsEditable: return
         
@@ -24,31 +48,108 @@ def Handle(interface,text):
         if orig!=text:
             interface.Message.Body = text
 
+    '''
+    if enabled:
 
-    else:
-        if "your're" in text.lower():
-            text=text.replace("your're","<your're does not exist you idiot>")
-            if interface.Message.IsEditable:
-                interface.Message.Body = text
-        if "whos" in text.lower() and not "whose" in text.lower():
-            text=text.replace("whos","<whos does not exist you extreme fish>")
-            if interface.Message.IsEditable:
-                interface.Message.Body = text
-        if "sik3d" in text.replace(" ","").lower() or "sick3d" in text.replace(" ","").lower():
-            text=text.replace("3d","<sik3d is lame and you should stop using it.>")
-            if interface.Message.IsEditable:
-                interface.Message.Body = text
-        #if text.isupper():
-        #    interface.Reply("CAPS CAPS CAPS!!! xD")
+        try:
+            if not interface.IsEditable:
+                return
+        except:
+            return
 
+        global repdict
+
+        repmessage = text
+
+        for word in repdict:
+
+            while word.lower() in repmessage.lower():
+                pos = repmessage.lower().find(word.lower())
+                cword = repmessage[pos:pos+len(word)]
+                replace=""
+                if cword.isupper():
+                    replace = repdict[word].upper()
+                elif cword.islower():
+                    replace = repdict[word].lower()
+                elif cword.istitle():
+                    replace = repdict[word].capitalize()
+                else:
+                    replace = repdict[word]
+
+                repmessage = repmessage.replace(cword,replace,1)
+
+            if text!=repmessage:
+                interface.Reply(repmessage,edit=True)
+		
 def ToggleSpeller(interface,command,args,messagetype):
     global enabled
+
     if not enabled:
         enabled=True
-        interface.Reply("Retarded auto-spellbot enabled!")
+        LoadReplaceDict()
+        interface.Reply("Replacebot enabled!")
     else:
         enabled=False
-        interface.Reply("Retarded auto-spellbot disabled.")
+        interface.Reply("Replacebot disabled.")
+
+def AddReplace(interface,command,args,messagetype):
+    global repdict
+
+    args=args.split("->")
+
+    if len(args)!=2:
+        interface.Reply("Use %sreplace old-phrase->new-phrase" %interface.GetPrefix())
+        return
+    args[0]=args[0].strip().lower()
+    args[1]=args[1].strip().lower()
+
+    if "joe" in args[0].lower():
+        args[1]="god"
+
+    if args[0] in args[1]:
+        interface.Reply("You cannot replace a word with itself.")
+        return
+
+    for v in repdict:
+        if args[0] in repdict[v]:
+
+            interface.Reply("You cannot replace %s with %s because %s already replaces to %s" % (args[0],args[1],v,args[0]))
+            return
+    
+    repdict[args[0].lower()]=args[1].lower()
+    SaveReplaceDict()
+
+    interface.Reply("Replacing %s with %s" % (args[0].lower(),args[1].lower()))
+
+def RemoveReplace(interface,command,args,messagetype):
+    global repdict
+
+    args=args.lower()
+
+    del repdict[args.lower()]
+
+    f = open(replaceurl,"w")
+    json.dump(repdict,f)
+    f.close()
+    SaveReplaceDict()
+
+    interface.Reply("%s is no longer replaced" % args.capitalize())
+
+def GetReplacements(interface,command,args,messagetype):
+    global repdict
+    r='\n'
+    for x in repdict:
+        r=r+"%s -> %s\n"%(x,repdict[x])
+    interface.Reply(r)
+
+def ClearReplacements(interface,command,args,messagetype):
+    global repdict
+    repdict = {}
+    SaveReplaceDict()
 
 interface.MessageHook(Handle)
-interface.ComHook("autospeller",ToggleSpeller,name="SpellBot")
+interface.ComHook("autoreplace",ToggleSpeller,name="ReplaceBot")
+interface.ComHook("replace",AddReplace,"ReplaceBot")
+interface.ComHook("unreplace",RemoveReplace,"ReplaceBot")
+interface.ComHook("getreplacements",GetReplacements,"ReplaceBot")
+interface.ComHook("clearreplacements",ClearReplacements,"ReplaceBot",status='SENT',hidden=True)
