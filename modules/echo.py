59ef97f06d3059455c8d71a71fc5d3fe04d62bd0
Spacerat <spacerat3004@gmail.com>
2010-06-12 13:13:33 +0100

    Added !hello

--- modules/echo.py
+++ modules/echo.py
@@ -1,7 +1,25 @@
 
 import interface
+import time
+
+global t
+t=0
 
 def Handle(interface,command,args,messagetype):
     interface.Reply(args)
 
-interface.AddHook("echo",Handle,name='EchoBot')
+def HelloHandle(interface,command,args,messagetype):
+
+    global t
+
+    if t+20>time.time():
+        return
+    t = time.time()
+
+    r = ''
+    for x in interface.Users:
+        r+=x+"! "
+    interface.Reply(r)
+
+interface.ComHook("hello",HelloHandle)
+interface.ComHook("echo",Handle,name='EchoBot')
