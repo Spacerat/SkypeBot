630e26b5ab1a06f10524f896b9e9027c9b1d5766
Spacerat <spacerat3004@gmail.com>
2010-07-14 03:24:56 +0100

    Logbot works with unicode characters.

--- modules/logbot.py
+++ modules/logbot.py
@@ -1,6 +1,7 @@
 
 import interface
 import re
+import codecs
 
 def Handle(it,text):
     # @type it ChatInterface
@@ -9,12 +10,16 @@ def Handle(it,text):
 
     filename = "data\\logs\\"+r.sub("",it.Message.ChatName)+".txt"
     try:
-        f = open(filename,"a+")
+        f = codecs.open(filename,"a+","utf-8")
     except:
         f = open(filename,"w")
         f.close()
-        f = open(filename,"a+")
-    f.write("[%s] %s: %s\n" % (it.Message.Datetime.strftime("%d/%m/%Y %H:%M:%S"),it.UserName,text))
+        #f = open(filename,"a+")
+        f = codecs.open(filename,"a+","utf-8")
+    
+    m = "[%s] %s: %s\n" % (it.Message.Datetime.strftime("%d/%m/%Y %H:%M:%S"),it.UserName,text)
+    #m=str(m)
+    f.write(m)
     f.close()
 
 
