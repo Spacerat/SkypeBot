c08b8792a3e0ab81c20f0ad6fa58d8768df6b620
Spacerat <spacerat3004@gmail.com>
2010-07-17 15:34:02 +0100

    Made wikibot work, and added a !guesswiki game.

--- modules/wikibot.py
+++ modules/wikibot.py
@@ -1,38 +1,104 @@
 
 import interface
+from BeautifulSoup import BeautifulSoup
+from stringsafety import *
 import urllib2
-import json
 import re
 
-def Handle(interface,command,args,messagetype):
-    # @type args str
+#A few bits and bobs from KittyBot once again. I, however, shall be making my life easier with BeautifulSoup.
+def load_url(url):
+    handle = urllib2.urlopen(urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10', 'Referrer': 'http://spacerat.meteornet.net'}))
+    data = handle.read().decode('utf-8')
+    handle.close()
+    return data
+
+def GetPage(args=''):
     args = args.replace(" ","%20")
 
-    url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles="+args
-    request = urllib2.Request(url,None,{'Referer':'http://spacerat.meteornet.net'})
-    response = urllib2.urlopen(request)
-    results = json.load(response)
-    if results:
-        if "pages" in results["query"]:
-            for page in results["query"]["pages"]:
-                if "revisions" in results["query"]["pages"][page]:
-                    p = re.compile(r'(\{\{).*?(\}\})<.*?>')
-                    text = results["query"]["pages"][page]["revisions"][0]['*']
-                    text = text.replace(chr(10),"")
-                    text = p.sub('',text)
-                    if text.startswith("#REDIRECT"):
-                        rd = re.compile(r'(#REDIRECT)*(\[)*(\])*')
-
-                        text = rd.sub('',text)
-                        Handle(interface,command,text,messagetype)
-                    else:
-                        rf = re.compile(r"(\[)*(\])*(')*")
-                        text = rf.sub('',text)
-                        interface.Reply( text.encode('utf-8')[0:400]+" ...")
-                 #   self.Reply(results["query"]["pages"][page]["revisions"][0]['*'].encode('utf-8').partition(chr(10))[0])
-        else:
-            interface.Reply("Wikipedia page for "+args+" does not exit.")
+    if len(args) == 0:
+        page = 'Special:Random'
     else:
-     print "no results"
+        page = args.replace(' ','_')
+    url = 'http://en.wikipedia.org/wiki/%s' % escapeurl(page)
+    try:
+        response = load_url(url)
+    except urllib2.HTTPError, e:
+        if e.code==404:
+            interface.Reply("There is no wiki page for %s"%args)
+            return
+        else:
+            interface.Reply("Error (%u) loading wiki page."%e.code)
+            return
 
-interface.ComHook("wiki",Handle,name="WikiBot")
+    doc = BeautifulSoup(response)
+    t = doc.table
+    if t: t.extract()
+
+    defin = doc.find("p")
+    o = FormatHTML(defin.renderContents())
+    o = re.sub('\[[0-9]*?\]','',o)
+    if len(o)>600: o=o[:597]+"..."
+    title = re.search('<title>(.+?) - Wikipedia, the free encyclopedia</title>', response).group(1) #yoink
+
+    return {'title':title,'content':o}
+
+
+def WikiHandle(interface,command,args,messagetype):
+
+    page = GetPage(args)
+    o = ("%s: %s"%(page['title'],page['content'].decode('utf-8')))
+    interface.Reply(o)
+
+
+global game
+game=None
+
+def GameHint():
+    global game
+    r = re.sub(game['title'],'*THING*', game['content'],re.IGNORECASE)
+    b = game['title'].partition(' (')[0]
+    c = game['title'].partition(', ')[0]
+    r = re.sub(b,'*THING*',r,re.IGNORECASE)
+    r = re.sub(c,'*THING*',r,re.IGNORECASE)
+    return r
+
+
+def GuessWiki(interface,command,args,message):
+    global game
+    if game==None:
+        game=GetPage()
+        interface.Reply("Type %sguesswiki title to guess what this is!"%interface.GetPrefix())
+        interface.Reply(GameHint())
+    else:
+        if args.strip()=='':
+            interface.Reply("Type %sguesswiki title to guess what this is!"%interface.GetPrefix())
+            interface.Reply(GameHint())
+        elif args.lower()==game['title'].lower() or args.lower()==game['title'].partition(' (')[0].lower():
+            interface.Reply("Correct!")
+            #interface.Reply("%s: %s"%(game['title'],game['content']))
+            game=None
+        else:
+            interface.Reply("Incorrect.")
+
+
+interface.ComHook("wiki",WikiHandle,name="WikiBot")
+interface.ComHook("guesswiki",GuessWiki,name="WikiGame")
+
+"""
+
+import interface
+from BeautifulSoup import BeautifulSoup
+from stringsafety import *
+import urllib2
+
+
+def Handle(interface,command,args,messagetype):
+    !ud phrase - Look up a phrase on urban dictionary.
+    url = "http://www.urbandictionary.com/"+escapeurl(args)
+    response = urllib2.urlopen(url)
+    doc = BeautifulSoup(response)
+    defin = doc.find("div",{"class":"definition"})
+    interface.Reply(args+" - "+FormatHTML(defin.renderContents()))
+
+interface.ComHook('ud',Handle,name='UrbanBot')
+"""
