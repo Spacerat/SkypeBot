e9d7e4df358361a05d95eafa9408c23cc864e202
Spacerat <spacerat3004@gmail.com>
2010-07-17 15:39:42 +0100

    The console is now another interface to communicate with the bot, and !skype, !irc, and !msn can be used to start individual components.

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,9 @@ import msnbot
 import skypebot
 import irc
 
+
+from modules.interface import RecieveMessage, DebugInterface, ComHook
+
 import modules.echo
 import modules.eightball
 import modules.google
@@ -24,15 +27,22 @@ import modules.scheduler
 import modules.hangman
 import modules.logbot
 import modules.skype
-import modules.interface
 import modules.animbot
 import modules.clearmessages
 import modules.rot13bot
 import modules.convokill
+import modules.skypeadmin
+import modules.securitycommands
+import modules.security
+import modules.omeglebot
+import modules.wikibot
 
 import threading
 
 import os.path
+
+import sys
+
 
 # ----------------------------------------------------------------------------------------------------
 # Modules setup
@@ -46,20 +56,40 @@ modules.spellbot.SetYahooID(open("data/yahooid.txt").readline())
 modules.correctbot.LoadReplaceDict("data/repdict.txt")
 modules.scheduler.AddInterface(modules.interface.DebugInterface())
 modules.convokill.LoadData("data/convokill.txt")
+modules.security.LoadData("data/access.txt")
 
+
+def StartSkype(i=None,command=None,args=None,messagetype=None):
+    """!skype Start the skype bot"""
+    skypebot.Init()
+def StartMSN(i=None,command=None,args=None,messagetype=None):
+    """!startmsn Start the msn bot"""
+    threading.Thread(None,msnbot.init).start()
+
+def StartIRC(i=None,command=None,args=None,messagetype=None):
+    args=args.split()
+    irc.IRC(irc.Network(server=args[0],nick='SpaceBot',ident='spacebot',realname="SpaceBot",channels=[args[1]] )).start()
+
+ComHook('skype',StartSkype,security=4)
+ComHook('msn',StartMSN,security=4)
+ComHook('irc',StartIRC,security=4)
 
 if __name__ == "__main__":
     #skypebot.Init()
     
-    Cmd = '';
-    while not Cmd == 'exit':
-        Cmd = raw_input('>');
+    cmd = '';
+    while not cmd == 'exit':
+        cmd = raw_input('>').decode('latin-1');
+        #try:
+        threading.Thread(RecieveMessage(DebugInterface(),cmd,'SENT')).start()
+        #except Exception as s:
+        #    print str(s)
+
+        '''
         if Cmd=='msn':
-            modules.interface.SetPrefix("!")
             threading.Thread(None,msnbot.init).start()
         elif Cmd=='irc':
-            modules.interface.SetPrefix(".")
-            irc.IRC(irc.Network(server='irc.calculasm.org',nick='JoeBot',ident='joebot',realname="Joe's bot",channels=["#mudkipz"] )).start()
+            irc.IRC(irc.Network(server='irc.blitzed.org',nick='SpaceBot',ident='spacebot',realname="SpaceBot",channels=["#blitzbasic"] )).start()
         elif Cmd=='skype':
-            modules.interface.SetPrefix("!")
             skypebot.Init()
+        '''
